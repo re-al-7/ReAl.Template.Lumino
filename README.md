b7bd735e2809bf8c97a3eb711e56656e0a690c18
R. Alonzo Vera A <7.re.al.7@gmail.com>
2017-11-16 16:41:06 -0400

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,13 @@
+# ReAl Template for Lumino
+
+[![Build status](https://ci.appveyor.com/api/projects/status/13vjue58ua69b4uc?svg=true)](https://ci.appveyor.com/project/re_al_/real-template-materialadmin)
+
+---------------------------------------
+
+Proyecto desarrollado en [.NET Core 2](https://www.microsoft.com/net/) para el uso del template [Lumino](https://medialoot.com/preview/lumino-premium/index.html).
+
+Ver el [historial](CHANGELOG.md) para los cambios realizados y las cosas por hacer.
+
+
+## License
+[MIT](LICENSE)
---
+++ ReAl.Template.Lumino/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReAl.Template.Lumino.Controllers
+{
+    [Authorize]
+    public class DashboardController : BaseController
+    {
+        // GET: Dashboard
+        public ActionResult Index(string app = "")
+        {
+            if (app != "")
+                HttpContext.Session.SetString("currentApp", app);
+
+            ViewBag.ListApp = this.getAplicaciones();
+            ViewBag.ListPages = this.getPages();
+            ViewData["Usuario"] = this.getUserName();
+
+            ViewData["app"] = "Your contact page.";
+            ViewData["Message"] = "Your contact page.";            
+            return View();
+        }
+    }
+}
