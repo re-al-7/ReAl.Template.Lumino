241f38b85abfa361645b1d56f92d921ee3fb92ed
R. Alonzo Vera A <7.re.al.7@gmail.com>
2017-11-17 23:56:16 -0400

    Adicionadas paginas Razor para el resto del Template

---
+++ ReAl.Template.Lumino/Pages/Template/Dashboard.cshtml.cs
@@ -0,0 +1,28 @@
+﻿// // <copyright file="Dashboard.cshtml.cs" company="INTEGRATE - Soluciones Informaticas">
+// // Copyright (c) 2016 Todos los derechos reservados
+// // </copyright>
+// // <author>re-al </author>
+// // <date>2017-11-16 21:42</date>
+
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ReAl.Template.Lumino.Dal.Entidades;
+using ReAl.Template.Lumino.Models;
+
+namespace ReAl.Template.Lumino.Pages
+{
+    public class DashboardModel : BasePageModel
+    {
+        public List<EntSegAplicaciones> ListApp { get; private set; }
+        public List<EntSegPaginas> ListPages { get; private set; }
+        public string Usuario { get; private set; }
+
+        public void OnGet()
+        {
+            ListApp = this.GetAplicaciones();
+            ListPages = this.GetPages();
+            Usuario = this.getUserName();
+        }
+    }
+}
---
+++ ReAl.Template.Lumino/Pages/Template/Panels.cshtml.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using ReAl.Template.Lumino.Dal.Entidades;
+using ReAl.Template.Lumino.Models;
+
+namespace ReAl.Template.Lumino.Pages.Template
+{
+    public class PanelsModel: BasePageModel
+    {
+        public List<EntSegAplicaciones> ListApp { get; private set; }
+        public List<EntSegPaginas> ListPages { get; private set; }
+        public string Usuario { get; private set; }
+
+        public void OnGet()
+        {
+            ListApp = this.GetAplicaciones();
+            ListPages = this.GetPages();
+            Usuario = this.getUserName();
+        }
+    }
+}
